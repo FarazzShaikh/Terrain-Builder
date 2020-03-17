f72e9357c22f1ef9b6bc81dc8b77c41e66b7e02d
Shaikh, Faraz Z <fzs3@hw.ac.uk>
2020-03-17 12:59:48 +0400

    ⭐️ feat: Integrated node

---
+++ public/javascripts/Plane.js
@@ -0,0 +1,127 @@
+import * as THREE from '../lib/three.js';
+import Perlin from '../lib/perlin.js'
+
+export default class Plane {
+
+    constructor() {
+        this.max = 0;
+        this.vertColorData = []
+        this.wSeg = this.hSeg = 256
+        this.geometry = new THREE.PlaneGeometry(20, 20, this.wSeg, this.hSeg);
+        // this.geometry = new THREE.BoxGeometry( 5, 5, 5, this.wSeg, this.hSeg)
+        this.material = new THREE.MeshPhysicalMaterial({ 
+            color: 0xffffff, 
+            flatShading: false,
+            wireframe: false, 
+            side: THREE.DoubleSide, 
+            vertexColors: THREE.VertexColors,
+
+           roughness: 1,
+           metalness: 0,
+
+        });
+        this.mesh = new THREE.Mesh(this.geometry, this.material);
+        this.mesh.rotation.x = Math.PI / 2 + Math.PI
+    }
+
+    displace(stylized) {
+
+        var pn = new Perlin(2);
+
+        let octaves = 7
+        let scale = 0.09
+        let persistance = 2
+        let lacunarity = 2
+
+
+        for (var i = 0; i < this.mesh.geometry.vertices.length; i++) {
+            let total = 0;
+            let frequency = 1;
+            let amplitude = 1;
+
+            let x = this.mesh.geometry.vertices[i].x
+            let y = this.mesh.geometry.vertices[i].y
+
+            for (var j = 0; j < octaves; j++) {
+
+                let noise = pn.noise(x / (scale * frequency), y / (scale * frequency), 0);
+                total += (noise * amplitude);
+                amplitude *= persistance;
+                frequency *= lacunarity;
+            }
+            this.mesh.geometry.vertices[i].z = total / 10 - 5
+            this.max = (total > this.max) ? total : this.max
+
+            this.vertColorData.push(total)
+
+        }
+
+        if(!stylized) {
+            this.geometry.computeVertexNormals();
+        }
+        
+        this.geometry.verticesNeedUpdate = true;
+    }
+
+    color(greyscale) {
+        this.geometry.computeBoundingBox();
+        let zMin = this.geometry.boundingBox.min.z;
+        let zMax = this.geometry.boundingBox.max.z;
+        let zRange = zMax - zMin;
+        let color, point, face, numberOfSides, vertexIndex;
+
+        // faces are indexed using characters
+        let faceIndices = ['a', 'b', 'c', 'd'];
+
+        // first, assign colors to vertices as desired
+        for (let i = 0; i < this.geometry.vertices.length; i++) {
+            point = this.geometry.vertices[i];
+
+            if(greyscale) {
+                color = new THREE.Color(0x000000);
+                let calc = 0.7 * (zMax - point.z) / zRange
+                color.setRGB(calc, calc, calc)
+            } else {
+                color = new THREE.Color(0x0000ff);
+                color.setHSL(0.7 * (zMax - point.z) / zRange, 1, 0.5);
+            }
+            this.geometry.colors[i] = color; // use this array for convenience
+        }
+        
+        // copy the colors as necessary to the face's vertexColors array.
+        for (let i = 0; i < this.geometry.faces.length; i++) {
+            face = this.geometry.faces[i];
+            numberOfSides = (face instanceof THREE.Face3) ? 3 : 4;
+            for (var j = 0; j < numberOfSides; j++) {
+                vertexIndex = face[faceIndices[j]];
+                face.vertexColors[j] = this.geometry.colors[vertexIndex];
+            }
+        }
+
+        
+
+    }
+
+    generateMap() {
+        var c = document.querySelector(".myCanvas");
+        c.width = c.height * (c.clientWidth / c.clientHeight);
+        var ctx = c.getContext("2d");
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, c.width, c.height);
+        var imgData = ctx.createImageData(this.wSeg+1, this.hSeg+1);
+
+        let mapData = new Uint8ClampedArray(imgData.data.length)
+        let dataind = 0;
+        for (var i = 0; i < this.geometry.colors.length; i+=1) {
+            mapData[dataind + 0] = this.geometry.colors[i].r * 255;
+            mapData[dataind + 1] = this.geometry.colors[i].g * 255;
+            mapData[dataind + 2] = this.geometry.colors[i].b * 255;
+            mapData[dataind + 3] = 255;
+            dataind+=4
+        }
+        for (let i = 0; i < imgData.data.length; i++) {
+            imgData.data[i] = mapData[i]   
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+}
---
+++ public/javascripts/main.js
@@ -0,0 +1,86 @@
+import * as THREE from '../lib/three.js';
+import { OrbitControls } from '../lib/OrbitControls.js'
+import Plane from './Plane.js'
+
+let scene, camera, renderer, planeMesh, controls
+
+let stylized = false
+let greyscale = true
+
+function initScene() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth / window.innerHeight, 0.1, 1000
+  );
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement)
+  camera.position.set(15, 15, 15)
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true
+  controls.dampingFactor = 0.25
+  controls.enableZoom = false
+  controls.enablePan = true
+  controls.enableZoom = true
+
+  controls.minPolarAngle = 0;
+  controls.maxPolarAngle = Math.PI / 2;
+
+  // let gridSize = 30;
+  // let gridDivisions = 100;
+  // let gridHelper = new THREE.GridHelper(gridSize, gridDivisions, true);
+  // scene.add(gridHelper);
+
+  let axesHelper = new THREE.AxesHelper(15);
+  scene.add(axesHelper);
+}
+
+function initLight() {
+  const light = new THREE.PointLight(0x404040, 3)
+  light.position.set(3, 3, 3)
+  light.rotation.set(1, 1, 1)
+  light.castShadow = true;
+  light.shadow.radius = 30;
+  scene.add(light)
+
+  const ambLight = new THREE.AmbientLight(0x404040, 0.9)
+  scene.add(ambLight)
+}
+
+function initGeometry() {
+  const plane = new Plane(32, 32)
+  planeMesh = plane.mesh
+  plane.displace(stylized);
+  plane.color(greyscale)
+  plane.generateMap()
+  scene.add(planeMesh);
+
+}
+
+function render() {
+  window.requestAnimationFrame(render);
+
+  if (planeMesh) {
+    // planeMesh.rotation.z += 0.005;
+  }
+
+  controls.update()
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+initScene()
+initLight()
+initGeometry()
+render();
+window.addEventListener('resize', onWindowResize, false);
