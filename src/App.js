5402f31222854e247228569578a538e7157158cd
Faraz Shaikh <farazzshaikh@gmail.com>
2021-04-12 14:06:10 +0530

    Added Hello world (Module 1)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+
+import * as THREE from "three";
 
 function App() {
+  const mount = useRef(null);
+
+  useEffect(() => {
+    console.clear();
+    console.log(mount);
+
+    const fov = 45;
+    const aspectRatio = window.innerWidth / window.innerHeight;
+    const nearPlane = 0.1;
+    const farPlane = 1000;
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      fov,
+      aspectRatio,
+      nearPlane,
+      farPlane
+    );
+
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: mount.current,
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    const animate = function () {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    };
+
+    animate();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <canvas width="500px" height="500px" ref={mount} />
     </div>
   );
 }
