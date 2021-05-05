e0afe5d40e5fd84313befb5e5b2a024dd69e343f
Faraz Shaikh <farazzshaikh@gmail.com>
2021-05-05 17:33:01 +0530

    Added Perlin noise and displacement

--- src/app/main.js
+++ src/app/main.js
@@ -1,20 +1,36 @@
 import * as THREE from "three";
 import setup from "./setup";
+import { Perlin } from "three-noise";
 
 export default function main(mount) {
   const scene = setup(mount);
 
   const geometry = new THREE.PlaneBufferGeometry(1, 1, 32, 32);
-  const material = new THREE.MeshNormalMaterial({ wireframe: true });
+  const material = new THREE.MeshNormalMaterial({
+    wireframe: false,
+    side: THREE.DoubleSide,
+  });
   const plane = new THREE.Mesh(geometry, material);
   plane.rotation.x = -Math.PI / 2;
 
   scene.add(plane);
 
-  const len = plane.geometry.attributes.position.array.length;
-  for (let i = 0; i < len; i += 3) {
-    plane.geometry.attributes.position.array[i + 2] = Math.random() * 0.1;
+  const perlin = new Perlin(Math.random());
+  const attr_position = plane.geometry.attributes.position;
+  const count = attr_position.count;
+  const newPositions = [];
+  for (let i = 0; i < count; i++) {
+    const position = new THREE.Vector3().fromBufferAttribute(attr_position, i);
+    const newPosition = position.clone();
+
+    position.multiplyScalar(3);
+    const noise = perlin.get2(position) * 0.5;
+
+    newPosition.z = noise;
+    newPositions.push(newPosition);
   }
+
+  plane.geometry.attributes.position.copyVector3sArray(newPositions);
   plane.geometry.attributes.position.needsUpdate = true;
   plane.geometry.computeVertexNormals();
 }
